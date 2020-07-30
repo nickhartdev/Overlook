2233e538edb6c90d0e6bd48741eda97d33327cff
Nick Hart <nick.hart.des@gmail.com>
2020-07-30 11:03:55 -0600

    Add name and id attributes

--- src/User.js
+++ src/User.js
@@ -1,5 +1,8 @@
 class User {
-
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+  }
 }
 
 export default User;
--- test/user-test.js
+++ test/user-test.js
@@ -2,13 +2,23 @@ import { expect } from 'chai';
 import User from '../src/User.js';
 
 describe('User', () => {
-  let user;
+  let user, testData;
 
   beforeEach(() => {
-    user = new User();
+    testData = {id: 1, name: 'Bill'}
+    user = new User(testData);
   })
 
   it('should be a function', () => {
     expect(User).to.be.a('function');
   })
+
+  it('should have an id', () => {
+    expect(user.id).to.equal(1);
+  })
+
+  it('should have a name', () => {
+    expect(user.name).to.equal('Bill');
+  })
+
 });
