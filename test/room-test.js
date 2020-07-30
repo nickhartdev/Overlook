a35e87716b5df0d7e2595e52646a084107055d4e
Nick Hart <nick.hart.des@gmail.com>
2020-07-30 13:49:21 -0600

    Add all room attributes

--- test/room-test.js
+++ test/room-test.js
@@ -2,10 +2,18 @@ import { expect } from 'chai';
 import Room from '../src/Room.js';
 
 describe('Room', () => {
-  let room, testRoomData;
+  let testRoomData, room;
 
   beforeEach(() => {
-    room = new Room();
+    testRoomData = {
+      number: 1,
+      roomType: 'residential suite',
+      bidet: false,
+      bedSize: 'california king',
+      numBeds: 1,
+      costPerNight: 500.00
+    }
+    room = new Room(testRoomData);
   })
 
   it('should be a function', () => {
@@ -18,5 +26,14 @@ describe('Room', () => {
 
   it('should be a child of Data', () => {
     expect(room.validateDataType).to.be.a('function');
+  })
+
+  it('should take in all attributes from a room data object', () => {
+    expect(room.number).to.equal(testRoomData.number);
+    expect(room.roomType).to.equal(testRoomData.roomType);
+    expect(room.bidet).to.equal(testRoomData.bidet);
+    expect(room.bedSize).to.equal(testRoomData.bedSize);
+    expect(room.numBeds).to.equal(testRoomData.numBeds);
+    expect(room.costPerNight).to.equal(testRoomData.costPerNight);
   })
 })
