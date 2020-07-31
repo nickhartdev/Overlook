7cb8099c9d1788f57830cdcbfd92264e8c03c167
Nick Hart <nick.hart.des@gmail.com>
2020-07-30 22:02:38 -0600

    Add user, room, and hotel fetch functions

---
+++ src/DOMUpdates.js
@@ -0,0 +1,6 @@
+class DOMUpdates {
+  displayData(data) {
+    const testDisplay = document.querySelector('.data');
+    testDisplay.innerText = data;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import './css/base.scss';
 import './images/turing-logo.png';
+import DOMUpdates from './DOMUpdates.js';
 
-retrieveUserData() {
+import User from './User.js';
+import Room from './Room.js';
+import Booking from './Booking.js';
+
+const retrieveUserData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
-  .then(response => response.json());
+  .then(response => response.json())
+  .then(dataSet => dataSet.users.map(user => new User(user)))
+  .then(userData => console.log(userData));
 }
 
-retrieveRoomData() {
+const retrieveRoomData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
-  .then(response => response.json());
+  .then(response => response.json())
+  .then(dataSet => dataSet.rooms.map(room => new Room(room)))
+  .then(roomData => console.log(roomData));
 }
 
-retrieveBookingData() {
+const retrieveBookingData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
-  .then(response => response.json());
+  .then(response => response.json())
+  .then(dataSet => dataSet.bookings.map(booking => new Booking(booking)))
+  .then(bookingsData => console.log(bookingsData))
 }
-
-getData() {
-  return Promise.all([this.retrieveUserData(), this.retrieveRoomData(), this.retrieveBookingData()])
-  .then(dataSets => dataSets);
-}
-
-export default scripts;
