e3325be0724782b278709af76021e35641cf303c
Nick Hart <nick.hart.des@gmail.com>
2020-07-31 15:50:12 -0600

    Console log appropriate responses for valid and invalid logins

--- src/DOMUpdates.js
+++ src/DOMUpdates.js
@@ -1,5 +1,31 @@
+import loginHandler from './loginHandler.js';
+
 const domUpdates = {
-  
+  clickHandler(event) {
+    if (event.target.id === 'log-in-btn') {
+      this.displayLoginResponse();
+    }
+  },
+
+  displayLoginResponse() {
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+
+    loginHandler.validateLogin(username, password) ? this.displayLandingPage() : this.displayError(username);
+  },
+
+  displayError() {
+    const usernameField = document.querySelector('#username');
+    const passwordField = document.querySelector('#password');
+
+    console.log('uh oh');
+    username.value = '';
+    password.value = '';
+  },
+
+  displayLandingPage(username) {
+    console.log('success!');
+  }
 }
 
 export default domUpdates;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import './css/base.scss';
-import './images/turing-logo.png';
 
 import User from './User.js';
 import Room from './Room.js';
 import Booking from './Booking.js';
 import domUpdates from './DOMUpdates.js';
 import dataFetcher from './dataFetcher.js';
-import login from './loginHandler.js';
+import loginHandler from './loginHandler.js';
 
 document.addEventListener('click', (event) => {
-  clickHandler(event);
-});
-
-function clickHandler(event) {
-  if (event.target.id === 'log-in-btn') {
-    validateLogIn();
-  }
-}
+  domUpdates.clickHandler(event);
+})
