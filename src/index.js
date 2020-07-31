852fd3504486d83fc175a169b0a5dffcb15e53e7
Nick Hart <nick.hart.des@gmail.com>
2020-07-30 21:16:33 -0600

    Move fetch functions to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './images/turing-logo.png';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+retrieveUserData() {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+  .then(response => response.json());
+}
+
+retrieveRoomData() {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+  .then(response => response.json());
+}
+
+retrieveBookingData() {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  .then(response => response.json());
+}
+
+getData() {
+  return Promise.all([this.retrieveUserData(), this.retrieveRoomData(), this.retrieveBookingData()])
+  .then(dataSets => dataSets);
+}
+
+export default scripts;
